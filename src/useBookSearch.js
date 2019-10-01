84697ea2825489ce1d20a3491812eb3b4154e9e1
beata <beata@emc2wave.com>
2019-10-01 11:30:26 -0700

    add axios fetch data

---
+++ src/useBookSearch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState} from 'react'
+import Axios from 'axios'
+
+export default function useBookSearch(query, pageNumber) {
+
+  useEffect(() => {
+    Axios({
+      method: 'GET',
+      url: 'http://openlibrary.org/search.json()',
+      params: { p: query, page: pageNumber}
+    }).then(res => {
+      console.log(data);
+    })
+  }, [query, pageNumber])
+  return null
+  
+}
