143f34a82bfa314207f84c2b456ae75868cf62a7
beata <beata@emc2wave.com>
2019-10-01 12:00:18 -0700

    change p onq Axios to axios

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -1,13 +1,13 @@
 import { useEffect, useState} from 'react'
-import Axios from 'axios'
+import axios from 'axios'
 
 export default function useBookSearch(query, pageNumber) {
 
   useEffect(() => {
-    Axios({
+    axios({
       method: 'GET',
-      url: 'http://openlibrary.org/search.json()',
-      params: { p: query, page: pageNumber}
+      url: 'http://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber}
     }).then(res => {
       console.log(res.data);
     })
